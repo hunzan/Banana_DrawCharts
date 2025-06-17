aef2eb849bc58cdfaf99db2d02ea3bdf3cd2596d
A-kâu Lîm <hunzan@gmail.com>
2025-06-18 02:48:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -10,5 +11,6 @@ def index():
 def csv_help():
     return render_template('csv_help.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
